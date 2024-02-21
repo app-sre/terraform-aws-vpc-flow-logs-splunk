5487c7608a2a851c756fee8e3bc4ca7e8cfac2ee
Rafael Porres Molina <rporresm@redhat.com>
2024-02-21 17:06:27 +0100

    Fix version and lock files (#14)
    It contained non needed modules from the time this module used a Lambda
    and it didn't set terraform binary minimum requirement.
    
    Signed-off-by: Rafa Porres Molina <rporresm@redhat.com>

--- versions.tf
+++ versions.tf
@@ -1,16 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">= 2.3.0, < 3.0.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0.0, < 6.0.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.2.0, < 4.0.0"
     }
   }
 }
